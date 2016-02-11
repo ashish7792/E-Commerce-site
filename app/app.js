2ed2ad281860835b555839b97bb98a88ab2e0d31
awaghma <arvind.waghmare@morningstar.com>
2016-02-11 22:19:04 +0530

    Integrated views

--- app/app.js
+++ app/app.js
@@ -2,14 +2,23 @@
 
 // Declare app level module which depends on views, and components
 
-angular.module('myApp', ['ngRoute', 'myApp.view1'])
+angular.module('furnitureWaleApp', ['ngRoute', 'furnitureWaleApp.landing'])
     .config(['$routeProvider', function($routeProvider) {
         $routeProvider
         .when('/landing', {
           templateUrl: 'landing-page/landing.html',
           controller: 'LandingCtrl'
-        }).
-        otherwise({redirectTo: '/landing'});
+        })
+        .when('/beds', {
+            templateUrl: 'product-variation/product-variation.html',
+            controller: 'LandingCtrl'
+        })
+        .when('/detailView', {
+            templateUrl: 'product-details/product-details.html',
+            controller: 'LandingCtrl'
+        })
+
+        .otherwise({redirectTo: '/landing'});
        }
     ]);
 
