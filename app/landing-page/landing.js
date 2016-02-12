50b6f9ed6fd6132bf2b74fed7638ebdb704a1071
awaghma <arvind.waghmare@morningstar.com>
2016-02-12 09:06:51 +0530

    some more changes

--- app/landing-page/landing.js
+++ app/landing-page/landing.js
@@ -1,8 +1,34 @@
 'use strict';
 
 angular.module('furnitureWaleApp.landing', [])
-
-
 .controller('LandingCtrl', [function() {
+    this.categoryList = [
+        {
+            "name" : "bed",
+            "imageUrl" : "category1"
+        },
+        {
+            "name" : "chairs",
+            "imageUrl" : "category/category1"
+        },
+        {
+            "name" : "tv base",
+            "imageUrl" : "category/category1"
+        },
+        {
+            "name" : "warddrobe",
+            "imageUrl" : "category/category1"
+        },
+        {
+            "name" : "sofa",
+            "imageUrl" : "category/category1"
+        },
+        {
+            "name" : "sofa",
+            "imageUrl" : "category/category1"
+        },
+
+    ]
+
 
 }]);
